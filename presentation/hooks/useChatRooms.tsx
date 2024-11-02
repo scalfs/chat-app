a46ea47b914bb897ca5b992bdfac2d373cd92185
Vitor Escalfoni <vitor.hme@gmail.com>
2024-11-01 22:14:12 -0300

    TL-02 feat: setting userId as query key and, adding useCallback

--- presentation/hooks/useChatRooms.tsx
+++ presentation/hooks/useChatRooms.tsx
@@ -1,35 +1,46 @@
 import { CreateChatRoomUseCaseImpl } from "@/application/use-cases/create-chat-room";
 import { CreateChatRoomInput } from "@/domain/use-cases/chat";
 import { queryClient } from "@/infrastructure/libs/tanstack-query";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  InvalidateOptions,
+  useMutation,
+  useQuery,
+} from "@tanstack/react-query";
+import { useCallback } from "react";
 import { useAuth } from "../providers/auth-provider";
 import { useDependencies } from "../providers/dependency-provider";
+
+const GET_CHAT_ROOMS_QUERY_KEY = "chat-rooms";
 
 export function useGetChatRooms() {
   const { user } = useAuth();
   const { chatRepository } = useDependencies();
 
   return useQuery({
-    queryKey: ["chat-rooms"],
+    queryKey: [GET_CHAT_ROOMS_QUERY_KEY, user?.id],
     queryFn: () => chatRepository.getChatRooms(user?.id || 0),
   });
 }
 
 export function useCreateChatRoom() {
+  const { user } = useAuth();
   const { chatRepository, userRepository } = useDependencies();
 
-  const createChatRoomUseCase = new CreateChatRoomUseCaseImpl(
-    userRepository,
-    chatRepository
-  );
+  const createChatRoom = useCallback(async (input: CreateChatRoomInput) => {
+    const createChatRoomUseCase = new CreateChatRoomUseCaseImpl(
+      userRepository,
+      chatRepository
+    );
+
+    return createChatRoomUseCase.execute(input);
+  }, []);
 
   return useMutation({
+    mutationFn: createChatRoom,
     mutationKey: ["create-chat-room"],
-    mutationFn: (input: CreateChatRoomInput) =>
-      createChatRoomUseCase.execute(input),
-    onSuccess: () => {
-      // Invalidate and refetch chat rooms after creating a new one
-      queryClient.invalidateQueries({ queryKey: ["chat-rooms"] });
-    },
+    onSuccess: () => clearChatRoomsCache(user?.id), // Invalidate and refetch chat rooms after creating a new one
   });
 }
+
+export const clearChatRoomsCache = (id?: number) =>
+  queryClient.invalidateQueries({ queryKey: [GET_CHAT_ROOMS_QUERY_KEY, id] });
