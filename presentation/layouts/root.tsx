1d2485093ec53b8d9bd5cc6f4c39cbab30b85632
Vitor Escalfoni <vitor.hme@gmail.com>
2024-11-05 09:44:19 -0300

    TL-04 chore: added react native gestures library

--- presentation/layouts/root.tsx
+++ presentation/layouts/root.tsx
@@ -7,18 +7,21 @@ import { AuthProvider } from "@/presentation/providers/auth-provider";
 import { DependencyProvider } from "@/presentation/providers/dependency-provider";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { PropsWithChildren } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export function RootLayout({ children: RootNav }: PropsWithChildren) {
   return (
     <GluestackUIProvider mode="dark">
       <ThemeProvider value={DarkTheme}>
-        <AuthProvider>
-          <DependencyProvider>
-            <QueryClientProvider client={queryClient}>
-              {RootNav}
-            </QueryClientProvider>
-          </DependencyProvider>
-        </AuthProvider>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <AuthProvider>
+            <DependencyProvider>
+              <QueryClientProvider client={queryClient}>
+                {RootNav}
+              </QueryClientProvider>
+            </DependencyProvider>
+          </AuthProvider>
+        </GestureHandlerRootView>
       </ThemeProvider>
     </GluestackUIProvider>
   );
